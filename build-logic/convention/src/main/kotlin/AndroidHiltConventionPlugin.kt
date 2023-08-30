ea98887de79e1f903d7f7bf152027debcbe86135
Sungyong An <sungyong.an@webtoonscorp.com>
2023-08-31 02:36:09 +0900

    migrate Dagger’s processors from kapt to ksp

--- build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
@@ -1,5 +1,5 @@
 import io.github.fornewid.dagger.hilt.buildlogic.implementation
-import io.github.fornewid.dagger.hilt.buildlogic.kapt
+import io.github.fornewid.dagger.hilt.buildlogic.ksp
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
@@ -12,13 +12,13 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("dagger.hilt.android.plugin")
-                apply("org.jetbrains.kotlin.kapt")
+                apply("com.google.devtools.ksp")
             }
 
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
             dependencies {
                 implementation(libs.findLibrary("dagger.hilt.android").get())
-                kapt(libs.findLibrary("dagger.hilt.compiler").get())
+                ksp(libs.findLibrary("dagger.hilt.compiler").get())
             }
         }
     }
