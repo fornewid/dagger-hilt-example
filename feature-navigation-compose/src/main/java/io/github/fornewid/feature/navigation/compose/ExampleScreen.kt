347d6e56bbd61f58f94541102b997acbe9a2ae0e
Sungyong An <sungyong.an@webtoonscorp.com>
2023-08-31 02:52:17 +0900

    Add sample codes for navigation-compose

--- feature-navigation-compose/src/main/java/io/github/fornewid/feature/navigation/compose/ExampleScreen.kt
+++ feature-navigation-compose/src/main/java/io/github/fornewid/feature/navigation/compose/ExampleScreen.kt
@@ -1,26 +1,35 @@
 package io.github.fornewid.feature.navigation.compose
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.ui.graphics.Color
 
 @Composable
-fun ExampleScreen(viewModel: ExampleNavGraphViewModel = hiltViewModel()) {
-    LaunchedEffect(Unit) {
-        viewModel.doSomething()
-    }
+fun ExampleScreen(
+    screenName: String,
+    viewModel: ExampleNavGraphViewModel,
+    onClick: (() -> Unit)? = null,
+) {
     Surface {
-        Box(
+        Column(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = "Hello World!")
+            Text(text = screenName)
+            Text(text = viewModel.getSomething(), color = Color.Red)
+            if (onClick != null) {
+                Button(onClick = onClick) {
+                    Text(text = "Go to next!")
+                }
+            }
         }
     }
 }
